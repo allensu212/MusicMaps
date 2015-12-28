83b5f9994a01309d9d05eb33087a6145aa0e89bc
Allen Su <Allen.Su@weightwatchers.com>
2015-12-28 19:33:43 +0800

    rename to MasterViewController

---
+++ MusicMaps/MasterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.swift
+//  MusicMaps
+//
+//  Created by Allen Su on 12/28/15.
+//  Copyright © 2015 Allen Su. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+class MasterViewController: UIViewController {
+
+}
+
